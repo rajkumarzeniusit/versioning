3fc78d4d203bf2abda697377d2fe771a5a3f0bff
raj123@zeniusit <raj.kumar@zeniusit.com>
2025-06-19 11:07:49 +0530

    update version.py

--- version.py
+++ version.py
@@ -1,8 +1,8 @@
 # This file is created by the pre-push script
 class Version:
     comment = "update version.py"
-    hash = "34962f024b0dfb7665c0f70bf0a63173d038726a"
-    version = "0.0.7"
+    hash = "a4e54203b4ff4844cc77362914d1f9be4ad12579"
+    version = "0.0.8"
 
 if __name__ == "__main__":
     print(Version.version)
