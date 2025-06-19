a95dbad446a252a65dcd2eaeaa89af41aa1a798f
raj123@zeniusit <raj.kumar@zeniusit.com>
2025-06-19 10:50:38 +0530

    added new version 3

--- version.py
+++ version.py
@@ -1,8 +1,8 @@
 # This file is created by the pre-push script
 class Version:
-    comment = "test auto version"
-    hash = "b8ca494ac49817a80b5b933c2c7762b2369f40b4"
-    version = "0.0.1"
+    comment = "added new version 1"
+    hash = "de9b287d3a967624df191f47314c03da54586d15"
+    version = "0.0.2"
 
 if __name__ == "__main__":
     print(Version.version)
