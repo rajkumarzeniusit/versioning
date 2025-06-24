4fb2cfb44f2940c698429693287a46d35cf6601a
raj123@zeniusit <raj.kumar@zeniusit.com>
2025-06-24 17:45:01 +0530

    54 version

--- version.py
+++ version.py
@@ -2,7 +2,7 @@
 class Version:
     comment = "trigger version update 53"
     hash = "86902da738ac1306194ac57e3195da476eeaec5e"
-    version = "0.0.53"
+    version = "v1.0.0.53"
 
 if __name__ == "__main__":
     print(Version.version)
