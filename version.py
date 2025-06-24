474722dd7639e7d98bf428984d6cf3aacbf7854d
raj123@zeniusit <raj.kumar@zeniusit.com>
2025-06-24 18:05:38 +0530

    update version.py

--- version.py
+++ version.py
@@ -1,8 +1,8 @@
 # This file is created by the version bump script
 class Version:
-    comment = "patch #minor"
-    hash = "0e15bc4c214f87e667f8277fb38e79449c3551b1"
-    version = "v1.1.0.0"
+    comment = "patch #major"
+    hash = "674d29707283c705ba6b345fef37757eeee97d96"
+    version = "v2.0.0.0"
 
 if __name__ == "__main__":
     print(Version.version)
